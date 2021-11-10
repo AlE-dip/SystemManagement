7e1eaa7414fee2dfd7b41d3625d858e69d0f6931
conglap <conglapp2606@gmail.com>
2021-11-10 16:45:56 +0700

    Update ProcessManager.java

--- src/main/java/client/ProcessManager.java
+++ src/main/java/client/ProcessManager.java
@@ -1,4 +1,31 @@
 package client;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ProcessManager {
+
+    private static final String TASKLIST = "tasklist";
+    private static final String KILL = "taskkill /F /PID ";
+
+    public static BufferedReader listProcess() throws Exception {
+
+        Process process = Runtime.getRuntime().exec(TASKLIST);
+        BufferedReader listProcess = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        return listProcess;
+    }
+
+    public static void killProcess(int PID) throws Exception {
+        Runtime.getRuntime().exec(KILL + PID);
+    }
+
+    public static void shutDown() throws Exception {
+        Runtime.getRuntime().exec("shutdown -s -t 0");
+    }
+
+    public static void main(String[] args) throws Exception{
+        listProcess();
+        killProcess(00000);
+        shutDown();
+    }
 }
