6c4c448f8155f4aa8c68508113d24d96d55f2c24
conglap2606 <conglapp2606@gmail.com>
2021-11-16 18:01:57 +0700

    Update ScreenshotPanel add scale img

--- src/main/java/oshi/demo/gui/ScreenshotPanel.java
+++ src/main/java/oshi/demo/gui/ScreenshotPanel.java
@@ -7,12 +7,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class ScreenshotPanel extends JPanel {
+public class ScreenshotPanel extends OshiJPanel {
     public ScreenshotPanel(String PathName) {
+        setSize(1000, 1000); //fitImage() cần phải đọc size của Panel
         try {
-            BufferedImage myPicture = ImageIO.read(new File(PathName));
-            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-            add(picLabel);
+            BufferedImage bi_Img = ImageIO.read(new File(PathName));
+            Image img = fitImage(bi_Img);
+            JLabel imgLabel = new JLabel(new ImageIcon(img));
+            add(imgLabel);
         } catch (IOException e) {
             System.out.println(e);
         }
@@ -26,12 +28,23 @@ public class ScreenshotPanel extends JPanel {
         }
     }
 
+    public Image fitImage (BufferedImage img) {
+        float HeSo = (float) img.getWidth()/img.getHeight();
+        float Width = this.getWidth();
+        float Height = Width/HeSo;
+        if (this.getHeight() < Height) {
+            Height = this.getHeight();
+            Width = Height*HeSo;
+        }
+        return img.getScaledInstance((int) Width, (int) Height, Image.SCALE_SMOOTH);
+    }
 
     public static void main (String[] args) throws Exception {
         JFrame frame = new JFrame();
         ScreenshotPanel screenshotPanel = new ScreenshotPanel("screenshot.png");
-        frame.setSize(1920,1030);
+        frame.setSize(1100,1100);
         frame.add(screenshotPanel);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
